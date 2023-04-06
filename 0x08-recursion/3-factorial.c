7bec7ce4a3975f40a75d13f1f135d92956bd3181
David Oyakhilome <davidoyakhilome15@gmail.com>
2023-04-05 20:48:00 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/*
+ * function - recursion
+ * @*s - argument
+ * returns factorial
+ */
+
+int factorial(int n)
+	if (n == 1) {
+		return (1);
+	} else {
+		return (n*factorial(n-1);
+				}
+
