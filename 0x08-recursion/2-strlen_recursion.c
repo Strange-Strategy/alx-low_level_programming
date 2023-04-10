89acd580bb3c48daab4c50f364112c1609dd0a77
David Oyakhilome <davidoyakhilome15@gmail.com>
2023-04-10 03:50:02 -0700

    stlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - prints the length of a string
+ * @s: string to print its length
+ *
+ * Return: string length
+ */
+int _strlen_recursion(char *s)
+{
+	int length;
+
+	if (*s != '\0')
+	{
+		length++;
+		length += _strlen_recursion(s + 1);
+	}
+	return (length);
+}
