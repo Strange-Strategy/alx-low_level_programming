8fe3423cf8a3fde2e5586e14e5ba321f5a902213
David Oyakhilome <davidoyakhilome15@gmail.com>
2023-04-10 16:17:22 -0700

    args done

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+#include "main.h"
 /**
- * main - prints number of argument passed
+ * main - print the number of arguments
  * @argc: count of arguments
- * @argv: arguments passed
+ * @argv: array of arguments
  * Return: Always 0
  */
-int main(int argc __attribute__((unused)), char *argv)
+int main(int argc, char *argv[])
 {
-	int numofags = argc;
+	(void)argv;
 
-	printf("num of args = %d\n", numofags);
+	printf("%d\n", argc - 1);
 	return (0);
 }
