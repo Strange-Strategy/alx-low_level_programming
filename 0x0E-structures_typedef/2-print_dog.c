b02398bd78eb3259b6717eabfca4608bfafa732a
David Oyakhilome <davidoyakhilome15@gmail.com>
2023-04-19 01:30:51 -0700

    probe

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,23 +12,23 @@ void print_dog(struct dog *d)
 	}
 	if (d->name == NULL)
 	{
-		printf("Name: %s", "(nil)");
+		printf("Name: %s\n", "(nil)");
 	} else
 	{
 		printf("Name: %s\n", d->name);
 	}
 
-	if (d->age == '0')
-	{
-		printf("Age: %s", "(nil)");
-	} else
+	if (d->age)
 	{
 		printf("Age: %.6f\n", d->age);
+	} else
+	{
+		printf("Age: %s\n", "(nil)");
 	}
 
 	if (d->owner == NULL)
 	{
-		printf("Owner: %s", "(nil)");
+		printf("Owner: %s\n", "(nil)");
 	} else
 	{
 		printf("Owner: %s\n", d->owner);
