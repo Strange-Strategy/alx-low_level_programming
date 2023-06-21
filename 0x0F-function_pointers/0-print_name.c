583c6658350fc2f5583a7cc38e55ad113edc2c49
Oyakhilome David <davidoyakhilome15@gmail.com>
2023-06-21 16:23:53 -0700

    printname

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * print_name - function to print a name
+ * @f: pointer a function that retuns void and takes in a char
+ * @name: name to print
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
