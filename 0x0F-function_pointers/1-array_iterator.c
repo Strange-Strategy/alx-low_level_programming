d534c49b70bfe139531ca2c327a66ec34f6ff69e
Oyakhilome David <davidoyakhilome15@gmail.com>
2023-07-03 01:57:32 -0700

    fpointer fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,6 +9,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t c;
 
+	if (array == NULL || action == NULL)
+		return;
+
 	for (c = 0; c < size; c++)
 	{
 		action(array[c]);
